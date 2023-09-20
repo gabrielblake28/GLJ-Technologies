1dddc9fe26507f520e427f81bcf2d304fe923dc1
gabrielblake28 <gabriel.blake28@gmail.com>
2023-09-19 17:52:44 -0700

    Added simple server with template for individual pages

---
+++ index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import * as path from "path";
+
+const app = express();
+const port = 4000;
+
+// Create a server instance
+
+app.use(express.static(path.join(__dirname, "src")));
+
+app.get("/lori", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "pages", "Lori.html"));
+});
+
+app.get("/jacob", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "pages", "Jacob.html"));
+});
+
+app.get("/gabriel", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "pages", "Gabriel.html"));
+});
+
+// Listen on port 3000
+app.listen(port, () => {
+  console.log(`local GLJ-Technologies listening on port ${port}`);
+});
